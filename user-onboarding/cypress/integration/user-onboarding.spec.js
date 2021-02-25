6eee14be0d051a23dd751f4446a34911fcfbe866
Alexis Marroquin <alexismarroquin754@gmail.com>
2021-02-25 13:56:15 -0800

    cypress test | can submit

--- user-onboarding/cypress/integration/user-onboarding.spec.js
+++ user-onboarding/cypress/integration/user-onboarding.spec.js
@@ -9,10 +9,13 @@ describe('User-OnBoarding Tests', () => {
         expect(true).to.equals(true)
     })
 
+    // Selecting Els
+
     const nameInput = () => cy.get('input[name=name]')
     const emailInput = () => cy.get('input[name=email]')
     const passwordInput = () => cy.get('input[name=password]')
     const tosInput = () => cy.get('input[name=tos]')
+    const submitButton = () => cy.get('form button')
 
     describe('Proper elements are showing', () => {
 
@@ -21,22 +24,35 @@ describe('User-OnBoarding Tests', () => {
             emailInput().should('exist')
             passwordInput().should('exist')
             tosInput().should('exist')
+            submitButton().should('exist')
         })
 
     })
 
-    describe('Typing inside inputs', () => {
+    describe('Submitting Form Success', () => {
         it('Fills new user form inputs', () => {
             // Filling inputs
             nameInput()
                 .type('John Doe')
             emailInput()
                 .type('johndoe@domiain.com')
-            // Make sure inputs have expected values
+            passwordInput()
+                .type('superSecurePassword123')
+            tosInput()
+                .click()
+            // Assert inputs have expected values
             nameInput()
                 .should('have.value', 'John Doe')
             emailInput()
                 .should('have.value', 'johndoe@domiain.com')
+            passwordInput()
+                .should('have.value', 'superSecurePassword123')
+            tosInput()
+                .should('be.checked')
+            submitButton()
+                .click()
+            submitButton()
+                .should('be.disabled')   
         })
     })
 
